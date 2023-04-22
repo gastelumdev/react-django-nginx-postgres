c48fdd0e7c2af211e6afbf1cc7b38b3d53d3ee81
Omar Gastelum <gastelumdev@gmail.com>
2023-04-22 09:59:45 -0700

    Add event detail class

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,9 +1,11 @@
 # from django.conf.urls import url
 from django.urls import path, include
 from .views import (
+    EventDetailApiView,
     EventListApiView,
 )
 
 urlpatterns = [
     path('', EventListApiView.as_view()),
+    path('<int:pk>/', EventDetailApiView.as_view()),
 ]
