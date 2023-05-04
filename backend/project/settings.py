b058d49dfac1c2340a0ead90d512100dd126cccc
Omar Gastelum <gastelumdev@gmail.com>
2023-05-04 09:30:03 -0700

    Fix Bug

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 from django.core.management.utils import get_random_secret_key
 import environ
 
@@ -23,8 +24,10 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = env(
-    'SECRET_KEY', "t+li4q6y4(9q!p9(_-s$!qhc280wue6^5!vb#ezunmlnh47hv(")
+# SECRET_KEY = env(
+#     'SECRET_KEY', "t+li4q6y4(9q!p9(_-s$!qhc280wue6^5!vb#ezunmlnh47hv(")
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
